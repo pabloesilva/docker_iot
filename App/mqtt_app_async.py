4a698d7023e97804a76dc25cd415df634437282b
pabloesilva <pablosilva.ten@gmail.com>
2025-04-10 16:41:19 -0300

    agregado de funcion distribuidora para manejar distintas colas en cada topico. cambio en la configuracion de logger. agregado de nombre para cada tarea creada (taskname).

--- App/mqtt_app_async.py
+++ App/mqtt_app_async.py
@@ -2,36 +2,56 @@ import asyncio
 import logging
 import os
 import ssl
-import aiomqtt 
+import aiomqtt
 
-# configuracion mensajes de logging 
-logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(name)s] %(message)s", datefmt='%d/%m/%Y %H:%M:%S')
+# Configuración de logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(taskName)s] %(message)s",
+    datefmt='%d/%m/%Y %H:%M:%S'
+)
 
-async def message_dispatcher(client, topics):
-    logger = logging.getLogger("subs")
-    for topic in topics:
-        await client.subscribe(topic)
-        logger.info(f"Suscripto a: {topic}")
-    
+# Colas para enrutar mensajes por tópico
+topic1_queue = asyncio.Queue()
+topic2_queue = asyncio.Queue()
+
+async def subscriber(client, topic):
+    await client.subscribe(topic)
+    logging.info(f"Suscripto a: {topic}")
+
+async def topic1_consumer():
+    while True:
+        message = await topic1_queue.get()
+        logging.info(f"{message.payload.decode()}")
+
+async def topic2_consumer():
+    while True:
+        message = await topic2_queue.get()
+        logging.info(f"{message.payload.decode()}")
+
+async def message_distributor(client):
     async for message in client.messages:
-        logger.info(f"Mensaje en '{message.topic}': {message.payload.decode()}")
+        if message.topic.matches(os.getenv("TOPICO1")):
+            topic1_queue.put_nowait(message)
+            logging.info(f"Enviando a cola de {os.getenv("TOPICO1")}")
+        elif message.topic.matches(os.getenv("TOPICO2")):
+            topic2_queue.put_nowait(message)
+            logging.info(f"Enviando a cola de {os.getenv("TOPICO2")}")
 
 async def counter_incrementer(counter, lock):
-    logger = logging.getLogger("incrementar")
     while True:
         await asyncio.sleep(3)
         async with lock:
             counter["value"] += 1
-            logger.info(f"Contador: {counter['value']}")
+            logging.info(f"Contador: {counter['value']}")
 
 async def counter_publisher(client, counter, lock, pub_topic):
-    logger = logging.getLogger("pubs")
     while True:
         await asyncio.sleep(5)
         async with lock:
             value = counter["value"]
         await client.publish(pub_topic, str(value))
-        logger.info(f"Contador publicado: {value}")
+        logging.info(f"Contador publicado: {value}")
 
 async def main():
     broker = os.getenv("SERVIDOR")
@@ -48,15 +68,20 @@ async def main():
     tls_context.load_default_certs()
 
     async with aiomqtt.Client(
-        os.environ['SERVIDOR'],
+        broker,
         port=8883,
         tls_context=tls_context,
     ) as client:
-        await asyncio.gather(
-        message_dispatcher(client, [topic1, topic2]),
-        counter_incrementer(counter, lock),
-        counter_publisher(client, counter, lock, pub_topic)
-        )
+
+        task_group = asyncio.TaskGroup()
+        async with task_group:
+            task_group.create_task(subscriber(client, topic1), name="sub1")
+            task_group.create_task(subscriber(client, topic2), name="sub2")
+            task_group.create_task(message_distributor(client), name="distribuidor")
+            task_group.create_task(topic1_consumer(), name="temperatura")
+            task_group.create_task(topic2_consumer(), name="humedad")
+            task_group.create_task(counter_incrementer(counter, lock), name="incrementar")
+            task_group.create_task(counter_publisher(client, counter, lock, pub_topic), name="pub1")
 
 if __name__ == "__main__":
     try:
