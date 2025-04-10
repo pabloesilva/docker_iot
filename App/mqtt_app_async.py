76d20060ec643a019ef5b30e14d6a9eea5975224
pabloesilva <pablosilva.ten@gmail.com>
2025-04-10 14:08:12 -0300

    v2

--- App/mqtt_app_async.py
+++ App/mqtt_app_async.py
@@ -1,28 +1,31 @@
 import asyncio
 import logging
 import os
-from aiomqtt import Client
+import ssl
+import aiomqtt 
 
 # configuracion mensajes de logging 
-logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(name)s] %(levelname)s: %(message)s")
+logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(name)s] %(message)s", datefmt='%d/%m/%Y %H:%M:%S')
 
-async def subscribe_topic(client, topic, name):
-    logger = logging.getLogger(name)
-    async with client.messages() as messages:
+async def message_dispatcher(client, topics):
+    logger = logging.getLogger("subs")
+    for topic in topics:
         await client.subscribe(topic)
-        async for message in messages:
-            logger.info(f"Mensaje recibido en '{topic}': {message.payload.decode()}")
+        logger.info(f"Suscripto a: {topic}")
+    
+    async for message in client.messages:
+        logger.info(f"Mensaje en '{message.topic}': {message.payload.decode()}")
 
 async def counter_incrementer(counter, lock):
-    logger = logging.getLogger("incrementer")
+    logger = logging.getLogger("incrementar")
     while True:
         await asyncio.sleep(3)
         async with lock:
             counter["value"] += 1
-            logger.info(f"Contador incrementado: {counter['value']}")
+            logger.info(f"Contador: {counter['value']}")
 
 async def counter_publisher(client, counter, lock, pub_topic):
-    logger = logging.getLogger("publisher")
+    logger = logging.getLogger("pubs")
     while True:
         await asyncio.sleep(5)
         async with lock:
@@ -39,12 +42,20 @@ async def main():
     counter = {"value": 0}
     lock = asyncio.Lock()
 
-    async with Client(broker, port=8883, tls_context=True) as client:
+    tls_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+    tls_context.verify_mode = ssl.CERT_REQUIRED
+    tls_context.check_hostname = True
+    tls_context.load_default_certs()
+
+    async with aiomqtt.Client(
+        os.environ['SERVIDOR'],
+        port=8883,
+        tls_context=tls_context,
+    ) as client:
         await asyncio.gather(
-            subscribe_topic(client, topic1, "SUB1"),
-            subscribe_topic(client, topic2, "SUB2"),
-            counter_incrementer(counter, lock),
-            counter_publisher(client, counter, lock, pub_topic)
+        message_dispatcher(client, [topic1, topic2]),
+        counter_incrementer(counter, lock),
+        counter_publisher(client, counter, lock, pub_topic)
         )
 
 if __name__ == "__main__":
